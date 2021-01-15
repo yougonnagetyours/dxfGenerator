893c193b80d091c85079ffd556ad58078d5dbcb0
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-01-15 11:32:52 +0000

    change circle paramethers

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ const fs = require('fs');
 
 let d = new Drawing();
 
-d.setUnits('Decimeters');
-d.drawText(10, 0, 10, 0, 'Hello World'); // draw text in the default layer named "0"
-d.addLayer('l_green', Drawing.ACI.GREEN, 'CONTINUOUS');
-d.setActiveLayer('l_green');
-d.drawText(20, -70, 10, 0, 'go green!');
-
-//or fluent
 d.addLayer('l_yellow', Drawing.ACI.YELLOW, 'DOTTED')
  .setActiveLayer('l_yellow')
- .drawCircle(50, -30, 25);
+ .drawCircle(0, 0, 25);
 
 fs.writeFileSync(__filename + '.dxf', d.toDxfString());
