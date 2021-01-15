8f0a791ab6395263fb657e86ac23be90e2c5579b
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-01-15 11:41:15 +0000

    Add another arc

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const fs = require('fs');
 
 let d = new Drawing();
 
-d.addLayer('l_yellow', Drawing.ACI.YELLOW, 'DOTTED')
+d.addLayer('l_yellow', Drawing.ACI.YELLOW, 'CONTINUOUS')
  .setActiveLayer('l_yellow')
- .drawCircle(0, 0, 25)
- .drawArc(0, 0, 5, 0, 180);
+ .drawCircle(0, 0, 25);
+
+d.addLayer('l_green', Drawing.ACI.GREEN, 'CONTINUOUS')
+ .setActiveLayer('l_yellow')
+ .drawArc(50, 50, 50, 30, 90);
 
 fs.writeFileSync(__filename + '.dxf', d.toDxfString());
