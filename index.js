89fe69cd74e884983e2fdba67d1e88f96cea6b0c
Michał Potoczny <michal.potoczny.92@gmail.com>
2021-01-15 11:52:04 +0000

    chnage layers

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ let d = new Drawing();
 
 d.addLayer('l_yellow', Drawing.ACI.YELLOW, 'CONTINUOUS')
  .setActiveLayer('l_yellow')
- .drawCircle(0, 0, 25);
-
-d.addLayer('l_green', Drawing.ACI.GREEN, 'CONTINUOUS')
- .setActiveLayer('l_green')
+ .drawCircle(0, 0, 25)
  .drawArc(50, 50, 50, 0, 90);
 
 fs.writeFileSync(__filename + '.dxf', d.toDxfString());
